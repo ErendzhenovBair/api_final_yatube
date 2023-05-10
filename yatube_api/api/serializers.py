8535a10b40bf1c3c42c05f3f50a2e8af951bfc7d
Bair Erendzhenov <erendzhenovbair1990@yandex.ru>
2023-05-10 16:09:20 +0300

    saved changes5

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 from rest_framework.relations import SlugRelatedField
 from rest_framework.serializers import ValidationError
 from rest_framework.validators import UniqueTogetherValidator
@@ -9,6 +10,7 @@ from posts.models import Comment, Follow, Group, Post
 User = get_user_model()
 
 FOLLOWING_NOT_FOUND_ERROR = 'username в following не найден'
+SELF_FOLLOW_ERROR = 'Нельзя подписаться на самого себя'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -20,6 +22,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -42,7 +45,8 @@ class PostSerializer(serializers.ModelSerializer):
 class FollowSerializer(serializers.ModelSerializer):
     """Сериализатор для подписок"""
     user = serializers.SlugRelatedField(
-        slug_field='username', read_only=True)
+        slug_field='username', read_only=True, default=CurrentUserDefault()
+    )
     following = serializers.SlugRelatedField(
         slug_field='username', queryset=User.objects.all(),
         allow_null=True
@@ -55,7 +59,7 @@ class FollowSerializer(serializers.ModelSerializer):
     validators = [
         UniqueTogetherValidator(
             queryset=Follow.objects.all(),
-            fields=('user', 'following'),
+            fields=['user', 'following'],
             message='Вы уже подписаны на этого пользователя'
         )
     ]
@@ -64,4 +68,6 @@ class FollowSerializer(serializers.ModelSerializer):
         following = User.objects.filter(username=value).first()
         if not following:
             raise ValidationError(FOLLOWING_NOT_FOUND_ERROR)
+        elif following == self.context['request'].user:
+            raise ValidationError(SELF_FOLLOW_ERROR)
         return following
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
@@ -12,6 +13,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly, ]
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -26,7 +28,9 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly
+    ]
 
     def get_post(self):
         return get_object_or_404(Post, id=self.kwargs.get('post_id'))
@@ -39,14 +43,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class FollowViewSet(viewsets.ModelViewSet):
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = [IsAuthenticated, ]
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('following__username', )
+    search_fields = ('following__username', 'user__username')
 
     def get_queryset(self):
-        return self.request.user.follower.all()
+        return Follow.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.is_valid()
